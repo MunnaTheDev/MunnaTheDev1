b73f6ff117f463e8e1a0bbb492e5532dfc75b190
MunnaTheDev <gamingwithmunna01@gmail.com>
2026-06-26 18:36:54 +0600

    run.sh

--- menu/run.sh
+++ menu/run.sh
@@ -88,12 +88,7 @@ if curl -fsSL -A "Bane-VIP-Agent" --netrc -o "$payload" "$URL"; then
     echo -e "\n"
    
    # Execute payload
-bash "$payload"
-
-echo -e "\n${P}★★★ REDIRECTING TO MUNNATHEDEV UI ★★★${NC}\n"
-sleep 1
-exec bash <(curl -fsSL https://raw.githubusercontent.com/MunnaTheDev/MunnaTheDev1/refs/heads/main/menu/UI.sh)
-    
+exec bash <(curl -fsSL https://raw.githubusercontent.com/MunnaTheDev/MunnaTheDev1/refs/heads/main/menu/UI.sh)    
 else
     echo -e "${R}FAILED${NC}"
     echo -e " ${DG}└─ Error Detail:${NC} ${R}Connection Terminated by Host${NC}"
